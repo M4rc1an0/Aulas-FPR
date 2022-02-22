7a4d7a21254770e77a20bccae61eb26fc1dc0219
Rogério Marciano <marciano.arantes1999@gmail.com>
2022-02-21 23:43:56 -0300

    estrutura

---
+++ estrutura.js
@@ -0,0 +1,47 @@
+const boolean = true
+const string = "Rogério"
+const number = 22
+const float = 10.2
+const undefined = undefined
+const nula = null
+const array = []
+const objeto = {}
+
+const frutas = [
+    {
+        nome: "Banana",
+        cor: "amarelo",
+        arvore: "bananeira",
+        bom_pra_caimbra: true,
+        bom_coração: true,
+        numero_fruta: 1,
+        tipos: {
+            type1: "nanica",
+            type2: "ouro"
+        }
+    },
+    {
+        nome: "Maça",
+        cor: "vermelho",
+        arvore: "macieira",
+        bom_pra_caimbra: false,
+        bom_coração: true,
+        numero_fruta: 2,
+        tipos: {
+            type1: "gala", 
+            type2: "fuji"
+        }
+    },
+    {
+        nome: "Pera",
+        cor: "verde",
+        arvore: "pereira",
+        bom_pra_caimbra: false,
+        bom_coração: false,
+        numero_fruta: 3,
+        tipos: {
+            type1: "portuguesa",
+            type2: "comice" 
+        }
+    },
+]
