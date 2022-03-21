497108e1b0802236b49e63773f23932766854b3e
unknown <x790321@gruposantander.com>
2022-03-21 19:43:41 -0300

    presencial4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,80 +2,63 @@ import './App.css';
 
 function App() {
 
-  //MAP SEMPRE É USADO PARA TRAZER DADOS DE UM ARRAY []
-
-  // fazer uma lista exibindo 5 pessoas com nome, idade, data de nascimento e nome da mae, e para 3 pessoas
-  // o nome do pai, porem quem nao tiver o pai nem exibe o titulo de pai
-
-  const pessoas = [
+  const eventosEsportivos = [
     {
-      nome: "Roberto",
-      idade: 20,
-      dataNasc: "08/07/2002",
-      tel: "(11)99875-6271",
-      nomeMae: "Alexia",
+      evento: 'Olimpiadas',
+      organizador: 'Toshiro Muto',
+      data: '15/04/1998',
+      modalidade: [
+        {
+          esporte: 'Futebol',
+          id: 1
+        },
+        {
+          esporte: 'Basquete',
+          id: 2
+        }
+      ]
     },
     {
-      nome: "Alice",
-      idade: 32,
-      dataNasc: "01/02/1990",
-      tel: "(32)95482-3023",
-      nomeMae: "Amanda",
-      nomePai: "Matheus",
+      evento: 'Esportes Bela Vaq',
+      organizador: 'Belinha',
+      data: '21/03/2022',
+      modalidade: [
+        {
+          esporte: 'Basquete',
+          id: 3
+        },
+        {
+          esporte: 'Futebol',
+          id: 4
+        }
+      ] 
     },
-    {
-      nome: "Renata",
-      idade: 22,
-      dataNasc: "23/05/1999",
-      tel: "(23)99735-1099",
-      nomeMae: "Bianca",
-      nomePai: "João",
-    },
-    {
-      nome: "Rafael",
-      idade: 25,
-      dataNasc: "23/05/1997",
-      tel: "(03)96489-0623",
-      nomeMae: "Natalia",
-      nomePai: "Guilherme",
-    },
-    {
-      nome: "Camila",
-      idade: 12,
-      dataNasc: "17/02/2010",
-      tel: "(11)99817-3304",
-      nomeMae: "Ana",
-    }
   ]
-
 
   return (
     <div className="App">
-      <div className='container'>
-        <div className='box'>
-          {pessoas.map(item => {
-            return (
-              <div>
-                <ul className='listagem'>
-                  <div>
-                    <li className='pessoa'><p>Nome:</p> {item.nome}</li>
-                    <li className='pessoa'><p>Idade:</p> {item.idade}</li>
-                    <li className='pessoa'><p>Data Nasc.:</p> {item.dataNasc}</li>
-                    <li className='pessoa'><p>Tel.:</p> {item.tel}</li>
-                    <li className='pessoa'><p>Nome Mãe:</p> {item.nomeMae}</li>
-                    {item.nomePai && <li className='pessoa'><p>Nome Pai:</p> {item.nomePai}</li>}
-                  </div>
-                  {item.nomePai ?
-                    <li className='titulo-pai'>TITULO DE PAI</li>
-                    :
-                    <li className='cade-pai'> :( </li>
-                  }
-                </ul>
-              </div>
-            )
-          })}
-        </div>
-      </div>
+      {
+       eventosEsportivos.map(eventos => {
+         const modalidade = eventos.modalidade.filter(esporteCerto => {
+           return esporteCerto.esporte !== 'Futebol'
+         }
+           
+         )
+         return (
+           <>
+           <h2>Evento:</h2>
+            <p>{eventos.evento}</p>
+            <h2>Organizador:</h2>
+            <p>{eventos.organizador}</p>
+            <h2>Data:</h2>
+            <p>{eventos.data}</p>
+            {modalidade.map(esporte => {
+              return <><h2>Esporte:</h2><p>{esporte.esporte}</p></>
+            })}
+           </>
+         )
+       }) 
+      }
     </div>
   );
 }
