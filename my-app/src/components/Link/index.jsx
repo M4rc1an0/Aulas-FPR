322ef9baaaa322d9abb937b72fa95d06b9749ccc
unknown <x790321@gruposantander.com>
2022-04-01 15:49:30 -0300

    ajustando-href

--- my-app/src/components/Link/index.jsx
+++ my-app/src/components/Link/index.jsx
@@ -4,7 +4,7 @@ export default function Link({ render }) {
     return (
         <div className="container">
             {render &&
-            <a>É U GELINHO</a>
+            <a href='https://www.youtube.com/watch?v=a8iZL2hI5WQ'>É U GELINHO</a>
             }
         </div>
     )
