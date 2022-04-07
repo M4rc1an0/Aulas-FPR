5acc5208f4134a9ae122d7312ca23d83caf0b559
Rogério Marciano <marciano.arantes1999@gmail.com>
2022-04-07 00:51:20 -0300

    função

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,81 +1,18 @@
 import './App.css';
+import { Console, Click } from './utils';
 
 function App() {
 
-  //MAP SEMPRE É USADO PARA TRAZER DADOS DE UM ARRAY []
+  // Criar uma função externa, que chame o numero digitado
+  // no input no console.log, acrescentando a mensagem --é muito louco--
 
-  // fazer uma lista exibindo 5 pessoas com nome, idade, data de nascimento e nome da mae, e para 3 pessoas
-  // o nome do pai, porem quem nao tiver o pai nem exibe o titulo de pai
-
-  const pessoas = [
-    {
-      nome: "Roberto",
-      idade: 20,
-      dataNasc: "08/07/2002",
-      tel: "(11)99875-6271",
-      nomeMae: "Alexia",
-    },
-    {
-      nome: "Alice",
-      idade: 32,
-      dataNasc: "01/02/1990",
-      tel: "(32)95482-3023",
-      nomeMae: "Amanda",
-      nomePai: "Matheus",
-    },
-    {
-      nome: "Renata",
-      idade: 22,
-      dataNasc: "23/05/1999",
-      tel: "(23)99735-1099",
-      nomeMae: "Bianca",
-      nomePai: "João",
-    },
-    {
-      nome: "Rafael",
-      idade: 25,
-      dataNasc: "23/05/1997",
-      tel: "(03)96489-0623",
-      nomeMae: "Natalia",
-      nomePai: "Guilherme",
-    },
-    {
-      nome: "Camila",
-      idade: 12,
-      dataNasc: "17/02/2010",
-      tel: "(11)99817-3304",
-      nomeMae: "Ana",
-    }
-  ]
-
+  // Passar duas variaveis para uma segunda função aonde elas retornem dentro de um
+  // alert depois do click
 
   return (
     <div className="App">
-      <div className='container'>
-        <div className='box'>
-          {pessoas.map(item => {
-            return (
-              <div>
-                <ul className='listagem'>
-                  <div>
-                    <li className='pessoa'><p>Nome:</p> {item.nome}</li>
-                    <li className='pessoa'><p>Idade:</p> {item.idade}</li>
-                    <li className='pessoa'><p>Data Nasc.:</p> {item.dataNasc}</li>
-                    <li className='pessoa'><p>Tel.:</p> {item.tel}</li>
-                    <li className='pessoa'><p>Nome Mãe:</p> {item.nomeMae}</li>
-                    {item.nomePai && <li className='pessoa'><p>Nome Pai:</p> {item.nomePai}</li>}
-                  </div>
-                  {item.nomePai ?
-                    <li className='titulo-pai'>TITULO DE PAI</li>
-                    :
-                    <li className='cade-pai'> :( </li>
-                  }
-                </ul>
-              </div>
-            )
-          })}
-        </div>
-      </div>
+      <input onChange={(e) => { Console(e.target.value) }} />
+      <button onClick={() => { Click() }}>Enviar</button>
     </div>
   );
 }
---
+++ my-app/src/utils/index.jsx
@@ -0,0 +1,10 @@
+export const Console = (valor) => {
+    console.log(valor, 'é muito louco')
+}
+
+const message = 'É U '
+const complement = 'GELINHO'
+
+export const Click = () => {
+    alert(message + complement)
+}
